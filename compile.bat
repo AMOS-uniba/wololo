43b05fdbd9ec3ccb47ae558982d720aa4549e2d1
TomasVoros <78957718+TomasVoros@users.noreply.github.com>
2024-11-21 14:57:29 +0100

    no icon is better than pyinstaller icon

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
 @echo Compiling Python file to EXE...
-pyinstaller.exe --onefile executables/convert-tree.py
-pyinstaller.exe --onefile executables/convert-single.py
+pyinstaller.exe --onefile --icon "NONE" convert-tree.py
+pyinstaller.exe --onefile --icon "NONE" convert-single.py
 @pause
